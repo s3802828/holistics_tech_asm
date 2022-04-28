121e7c8673900379e53f1473ea27fc6ab1d2994c
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-04-28 23:26:53 +0700

    deployed

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/ledis/' // Thay tên repository của các bạn vào đây nhé
+      ? '/holistic_tech_asm/' // Thay tên repository của các bạn vào đây nhé
       : '/'
   }
   
