f31c86527ccbdeb63073cf0ab9dadbb01aa1971a
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-04-28 12:05:32 +0700

    done data expiration

--- ledis/src/components/CommandBox.vue
+++ ledis/src/components/CommandBox.vue
@@ -33,6 +33,7 @@ export default {
             input: '',
             conversation: [],
             stored_data: {},
+            expiration: {}
         }
     },
     mounted() {
@@ -54,21 +55,49 @@ export default {
             let input_split = this.input.split(" ")
             var req_res = { id: this.conversation.length + 1, req: this.input }
             if (input_split[0].toUpperCase() === "SET") {
-                this.setCommand(input_split[1], input_split[2])
+                this.set(input_split[1], input_split[2])
                 req_res["res"] = "OK"
             }
             if (input_split[0].toUpperCase() === "GET") {
-                req_res["res"] = this.getCommand(input_split[1])
+                req_res["res"] = this.get(input_split[1])
             }
             if (input_split[0].toUpperCase() === "SADD") {
                 for (let index = 2; index < input_split.length; index++) {
-                    req_res["res"] = "Current length of this set: " + this.saddCommand(input_split[1], input_split[index])
+                    req_res["res"] = "Current length of this set: " + this.sadd(input_split[1], input_split[index])
                 }
             }
             if (input_split[0].toUpperCase() === "SREM") {
                 for (let index = 2; index < input_split.length; index++) {
-                    req_res["res"] = "Current length of this set: " + this.sremCommand(input_split[1], input_split[index])
+                    req_res["res"] = "Current length of this set: " + this.srem(input_split[1], input_split[index])
                 }
+            }
+            if (input_split[0].toUpperCase() === "SMEMBERS") {
+                const result = this.smembers(input_split[1])
+                let res = ''
+                for (let index = 0; index < result.length; index++) {
+                    res += result[index] + ' '
+                }
+                req_res["res"] = res
+            }
+
+            if (input_split[0].toUpperCase() === "SINTER") {
+                req_res["res"] = this.sinter(input_split.splice(1))
+            }
+
+            if (input_split[0].toUpperCase() === "KEYS") {
+                req_res["res"] = this.keys()
+            }
+
+            if (input_split[0].toUpperCase() === "DEL") {
+                this.del(input_split[1])
+                req_res["res"] = "OK"
+            }
+            if (input_split[0].toUpperCase() === "EXPIRE") {
+                this.expire(input_split[1], input_split[2])
+                req_res["res"] = "OK"
+            }
+            if (input_split[0].toUpperCase() === "TTL") {
+                req_res["res"] = this.ttl(input_split[1])
             }
             this.conversation.push(req_res)
             this.input = ''
@@ -80,15 +109,15 @@ export default {
             localStorage.setItem("stored_data", string_result)
         },
 
-        setCommand(key, value) {
+        set(key, value) {
             this.stored_data[key] = value
         },
 
-        getCommand(key) {
+        get(key) {
             return this.stored_data[key]
         },
 
-        saddCommand(key, value) {
+        sadd(key, value) {
             if (!this.stored_data[key]) {
                 this.stored_data[key] = []
             }
@@ -96,9 +125,61 @@ export default {
             return this.stored_data[key].length
         },
 
-        sremCommand(key, value) {
+        srem(key, value) {
             this.stored_data[key] = this.stored_data[key].filter((element) => element !== value)
             return this.stored_data[key].length
+        },
+
+        smembers(key) {
+            return this.stored_data[key]
+        },
+        sinter(key) {
+            let result = []
+            const element = this.stored_data[key[0]]
+            element.forEach(ele => {
+                let count = 0
+                for (let index = 1; index < key.length; index++) {
+                    const key_value = this.stored_data[key[index]];
+                    if (key_value.includes(ele)) {
+                        count += 1
+                    }
+                }
+                if (count == key.length - 1) {
+                    result.push(ele)
+                }
+            });
+            return result
+        },
+
+        keys() {
+            return Object.keys(this.stored_data)
+        },
+
+        del(key) {
+            delete this.stored_data[key]
+        },
+        expire: function(key, seconds) {
+            var currentTime = Date.now()
+
+            let interval = setInterval(() => {
+                console.log((seconds - getTimeLeft()) + "s")
+                this.expiration[key] = (seconds - getTimeLeft()) + "s"
+                if (getTimeLeft() >= seconds){
+                    this.del(key)
+                    delete this.expiration[key]
+                    clearInterval(interval)
+                }
+            }, 1000);
+
+            function getTimeLeft() {
+                return Math.ceil((Date.now() - currentTime) / 1000);
+            }
+        },
+        ttl(key){
+            if(!this.expiration[key]){
+                return "null"
+            }
+            return this.expiration[key]
         }
     },
 }
